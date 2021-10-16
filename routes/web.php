28320c134275409face66d1a830ef23f62fb5383
Diskaoktavaia <diskaoktavia36@gmail.com>
2021-10-17 01:25:21 +0700

    menampilkan data students

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 
 /*
@@ -21,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('students', StudentController::class);
