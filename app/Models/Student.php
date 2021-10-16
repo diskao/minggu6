57c4e58be65d774dbf4a617189f75b0197c20709
Diskaoktavaia <diskaoktavia36@gmail.com>
2021-10-17 01:51:46 +0700

    menambahkan fungsi tambah data baru

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     //use HasFactory;
-    protected $fillable = ['nim', 'name', 'class', 'departmen', 'phone_number'];
+    protected $fillable = ['nim', 'name', 'class', 'department', 'phone_number'];
 }
